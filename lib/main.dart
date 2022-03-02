cb5a19f4911fbae81a2b4b7430742758bc503ad7
ahmed <ahmedfetni277@gmail.com>
2022-03-02 15:00:19 +0000

    turning the app into a quiz game

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,32 @@ void main() {
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
   //final appBarBackgroudClor = Color(0x000000);
+  void answerQuestion() => print("we got ur answer");
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          // backgroundColor: appBarBackgroudClor,
-          title: const Center(child: Text("the first app i build ndnd ")),
-        ),
-        body: const Center(
-            child: Text("this is my default text that i'll put right here")),
-      ),
+          appBar: AppBar(
+            // backgroundColor: appBarBackgroudClor,
+            title: const Center(child: Text("the first app i build ndnd ")),
+          ),
+          body: Column(
+            children: <Widget>[
+              const Text("what is ur favorite color"),
+              RaisedButton(
+                child: Text("Blue"),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text("Red"),
+                onPressed: () => print("he said red"),
+              ),
+              RaisedButton(
+                child: const Text("Green"),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }
