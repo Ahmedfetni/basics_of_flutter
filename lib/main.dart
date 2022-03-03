d35d7bc54adc80d1afca1a4e0d17fde891da9e96
ahmed <ahmedfetni277@gmail.com>
2022-03-03 15:11:03 +0000

    changes made to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,61 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
-  App({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+  //App({Key? key}) : super(key: key);
+
+}
+
+class AppState extends State<App> {
   //final appBarBackgroudClor = Color(0x000000);
-  void answerQuestion() => print("we got ur answer");
+  var index = 0;
+  var questions = ["what is ur favorite color?", "what is ur favorite animal?"];
+  var button1 = ["Blue", "fish"];
+  var button2 = ["Red", "cat"];
+  var button3 = ["Green", "dog"];
+  void answerQuestion() {
+    setState(() {
+      if (index == 0) {
+        index = 1;
+      } else {
+        index = 0;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             // backgroundColor: appBarBackgroudClor,
-            title: const Center(child: Text("the first app i build ndnd ")),
+            title: const Center(
+                child: Text(
+              "the first app i build ndnd ",
+            )),
           ),
           body: Column(
             children: <Widget>[
-              const Text("what is ur favorite color"),
+              Question(questions.elementAt(index)),
               RaisedButton(
-                child: Text("Blue"),
+                child: Text(button1.elementAt(index)),
                 onPressed: answerQuestion,
               ),
               RaisedButton(
-                child: Text("Red"),
-                onPressed: () => print("he said red"),
+                child: Text(button2.elementAt(index)),
+                onPressed: answerQuestion,
               ),
               RaisedButton(
-                child: const Text("Green"),
+                child: Text(button3.elementAt(index)),
                 onPressed: answerQuestion,
               ),
             ],
